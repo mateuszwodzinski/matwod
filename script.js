fecc38378f0ea2a1ccb48cdd72926bd96f869cc8
mateuszwodzinski <47844900+mateuszwodzinski@users.noreply.github.com>
2019-03-06 11:01:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggle1(sDivId1) {
+    var oDiv1 = document.getElementById(sDivId1);
+    oDiv1.style.display = (oDiv1.style.display == "block") ? "none" : "block";
+}
+function toggle2(sDivId2) {
+    var oDiv2 = document.getElementById(sDivId2);
+    oDiv2.style.display = (oDiv2.style.display == "block") ? "none" : "block";
+}
+function toggle3(sDivId3) {
+    var oDiv3 = document.getElementById(sDivId3);
+    oDiv3.style.display = (oDiv3.style.display == "block") ? "none" : "block";
+}
+function toggle4(sDivId4) {
+    var oDiv4 = document.getElementById(sDivId4);
+    oDiv4.style.display = (oDiv4.style.display == "block") ? "none" : "block";
+}
